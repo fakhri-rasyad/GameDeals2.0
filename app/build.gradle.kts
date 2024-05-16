eb873c5beccea4bfd6e4adf8d6bf499da35a6c4f
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-05-16 19:04:13 +0800

    feat: network connectivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,12 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -42,6 +48,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    //Network
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/d121211017/gamedealsnew/ui/MainActivity.kt
+++ app/src/main/java/com/d121211017/gamedealsnew/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.d121211017.gamedealsnew
+package com.d121211017.gamedealsnew.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.d121211017.gamedealsnew.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

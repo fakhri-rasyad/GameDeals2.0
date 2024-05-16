c09924d121841ffc61b8f708ebfcfb28ad768791
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-05-16 20:21:26 +0800

    feat: repository injection

---
+++ app/src/main/java/com/d121211017/gamedealsnew/data/Injection.kt
@@ -0,0 +1,13 @@
+package com.d121211017.gamedealsnew.data
+
+import android.content.Context
+import com.d121211017.gamedealsnew.data.repository.GameDealsRepository
+import com.d121211017.gamedealsnew.data.retrofit.ApiConfig
+import com.d121211017.gamedealsnew.data.retrofit.ApiService
+
+object Injection {
+    fun getRepository(context: Context) : GameDealsRepository{
+        val apiService = ApiConfig.getApiService()
+        return GameDealsRepository(apiService)
+    }
+}
---
+++ app/src/main/java/com/d121211017/gamedealsnew/data/repository/GameDealsRepository.kt
@@ -0,0 +1,24 @@
+package com.d121211017.gamedealsnew.data.repository
+
+import com.d121211017.gamedealsnew.data.retrofit.ApiService
+
+class GameDealsRepository(
+    private val apiService: ApiService
+) {
+    suspend fun getDealsList() = apiService.getCurrentDeals()
+
+    private companion object {
+        @Volatile
+        private var INSTANCE : GameDealsRepository? = null
+
+        fun getInstance(
+            apiService: ApiService
+        ) : GameDealsRepository {
+                return INSTANCE ?: synchronized(this){
+                    val instance = GameDealsRepository(apiService = apiService)
+                    INSTANCE = instance
+                    instance
+                }
+        }
+    }
+}
